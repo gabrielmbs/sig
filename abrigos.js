8caa7654734752022f1fd143f1c28e40c7f566fc
Gabriel Souto <gabriel.souto@ccc.ufcg.edu.br>
2021-05-03 22:27:51 -0300

    primeiro commit

---
+++ abrigos.js
@@ -0,0 +1,50 @@
+const abrigoList = [
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-35.9161558, -7.2259468],
+    },
+    properties: {
+      name: "Centro de Zoonoses",
+      address: "Colocar enderenço",
+      phone: "83 99999-9999",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-35.8872992, -7.2181079],
+    },
+    properties: {
+      name: "Abrigo do Centro",
+      address: "Colocar enderenço",
+      phone: "83 2323 2323",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-35.8780337, -7.2288181],
+    },
+    properties: {
+      name: "Abrigo do Catolé",
+      address: "Colocar enderenço",
+      phone: "23 3424 4322",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-35.9022798, -7.3630898],
+    },
+    properties: {
+      name: "Abrigo de Queimadas",
+      address: "Colocar enderenço",
+      phone: "83 94388 4388",
+    },
+  },
+];
