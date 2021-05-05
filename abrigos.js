46bb86b39ff5c7f7770a3770af47a90915056a58
Gabriel Souto <gabriel.souto@ccc.ufcg.edu.br>
2021-05-05 02:26:39 -0300

    versão base

--- abrigos.js
+++ abrigos.js
@@ -7,8 +7,22 @@ const abrigoList = [
     },
     properties: {
       name: "Centro de Zoonoses",
-      address: "Colocar enderenço",
-      phone: "83 99999-9999",
+      address:
+        "R. Isolda Barros Torquarto, s/n - Bodocongó, Campina Grande - PB, 58430-090",
+      phone: "83 3310 7062",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-35.9063297, -7.2291347],
+    },
+    properties: {
+      name: "Abrigo do Centenário",
+      address:
+        "Av. Mal. Floriano Peixoto, 1806 - Centenário, Campina Grande - PB, 26210-000",
+      phone: "83 4002 8922",
     },
   },
   {
@@ -19,7 +33,7 @@ const abrigoList = [
     },
     properties: {
       name: "Abrigo do Centro",
-      address: "Colocar enderenço",
+      address: "R. Félix Araújo - Centro, Campina Grande - PB, 58400-078",
       phone: "83 2323 2323",
     },
   },
@@ -31,7 +45,7 @@ const abrigoList = [
     },
     properties: {
       name: "Abrigo do Catolé",
-      address: "Colocar enderenço",
+      address: "R. Arius, 16 - Catolé, Campina Grande - PB, 58410-284",
       phone: "23 3424 4322",
     },
   },
@@ -43,7 +57,7 @@ const abrigoList = [
     },
     properties: {
       name: "Abrigo de Queimadas",
-      address: "Colocar enderenço",
+      address: "Av. Severino Bezerra Cabral, 59, Queimadas - PB, 58475-000",
       phone: "83 94388 4388",
     },
   },
--- app.js
+++ app.js
@@ -1,113 +1,40 @@
-//Cria o mapa centralizado na UFCG
+const latlngUFCG = [-7.2153658, -35.9096925];
 const mymap = L.map("map", {
-  center: [-7.2153658, -35.9096925],
-  zoom: 18,
+  center: latlngUFCG,
+  zoom: 17,
 });
-//Link do mapbox, necessario colocar token
-const myToken =
-  "pk.eyJ1IjoiZ2FicmllbG1icyIsImEiOiJja280dTF4NHowZDY3Mm9tem5nMjQ5cW1nIn0.3g5SU3I9U6-BwkvN2aJRrQ";
-const tileUrl = `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${myToken}`;
+
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const attribution =
-  'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-//Criar camada de fundo
-const camadaSimples = L.tileLayer(tileUrl, {
-  id: "mapbox/light-v9",
-  tileSize: 512,
-  zoomOffset: -1,
+const openstreetmap = L.tileLayer(tileUrl, {
   attribution: attribution,
-});
-const camadaDetalhada = L.tileLayer(tileUrl, {
-  id: "mapbox/streets-v11",
-  tileSize: 512,
-  zoomOffset: -1,
-  attribution: attribution,
-});
-//setando camada inicial
-camadaDetalhada.addTo(mymap);
+}).addTo(mymap);
 
-// Criando lista de abrigos
-function generateList() {
-  const ul = document.querySelector(".list");
-  abrigoList.forEach((abrigo) => {
-    const li = document.createElement("li");
-    const div = document.createElement("div");
-    const a = document.createElement("a");
-    const p = document.createElement("p");
-    a.addEventListener("click", () => {
-      irParaAbrigo(abrigo);
-    });
-    div.classList.add("abrigo-item");
-    a.innerText = abrigo.properties.name;
-    a.href = "#";
-    p.innerText = abrigo.properties.address;
-
-    div.appendChild(a);
-    div.appendChild(p);
-    li.appendChild(div);
-    ul.appendChild(li);
-  });
-}
-generateList();
-
-//Função ir até o abrigo no mapa, duration é o tempo da animação
-function irParaAbrigo(abrigo) {
-  const lat = abrigo.geometry.coordinates[0];
-  const lng = abrigo.geometry.coordinates[1];
-  mymap.flyTo([lng, lat], 18, {
-    duration: 3,
-  });
-  setTimeout(() => {
-    L.popup({ closeButton: false, offset: L.point(0, -18) })
-      .setLatLng([lng, lat])
-      .setContent(makePopupContent(abrigo))
-      .openOn(mymap);
-  }, 3000);
-}
-
-//Criando conteudo dos popup
-function makePopupContent(abrigo) {
-  return `
-      <div>
-          <h4>${abrigo.properties.name}</h4>
-          <p>${abrigo.properties.address}</p>
-          <div class="phone-number">
-              <a href="tel:${abrigo.properties.phone}">${abrigo.properties.phone}</a>
-          </div>
-      </div>
-    `;
-}
-//Criando o Popup para cada abrigo
 function onEachFeature(feature, layer) {
-  layer.bindPopup(makePopupContent(feature), {
-    closeButton: false,
-    offset: L.point(0, -8),
-  });
+  layer.bindPopup(`<h4>${feature.properties.name}</h4>`);
 }
-//Criando o Icone personalizado
+
 const myIcon = L.icon({
   iconUrl: "abrigo.svg",
   iconSize: [40, 50],
 });
-//Criando camada de abrigos com seus Popups a partir do array de geoJsons
+// //Criando camada de abrigos com seus Popups a partir do array de geoJsons
 const abrigosLayer = L.geoJSON(abrigoList, {
   onEachFeature: onEachFeature,
   pointToLayer: function (feature, latlng) {
     return L.marker(latlng, { icon: myIcon });
   },
 });
-//Adiciono a camada ao mapa
+
 abrigosLayer.addTo(mymap);
 
-//Criando as opções de fundos para mapa, tipo radio só pode um
 const baseMaps = {
-  "<span style='color: gray'>Simples</span>": camadaSimples,
-  Detalhado: camadaDetalhada,
+  OpenStreetMap: openstreetmap,
 };
-//Criando as camadas que vão sobrepor a camada de fundo, checkbox pode varios
 const overlayMaps = {
   Abrigos: abrigosLayer,
 };
-//Adiciono ao mapa o controle sobre as camadas, pode ser apenas um,
-//  mas é necessario passar null no primeiro para ignorar
+
 L.control.layers(baseMaps, overlayMaps).addTo(mymap);
